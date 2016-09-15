d9de8b4c338235cd10bc402bb767eae0454a9262
Andy Bhadai <abhadai@gmail.com>
2016-09-15 16:09:08 +0200

    Started making models

---
+++ DatabaseManagementTool/DatabaseManagementTool/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseManagementTool
+{
+    class Employee
+    {
+        public int BSN;
+        public string Name;
+        public string Surname;
+
+        public Employee(int bsn, string name, string surname)
+        {
+            this.BSN = bsn;
+            this.Name = name;
+            this.Surname = surname;
+        }
+    }
+}
