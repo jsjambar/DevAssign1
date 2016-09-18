2c2ff57da66ff940f9e58fe4da62ebb16717e0f3
Andy Bhadai <abhadai@gmail.com>
2016-09-18 17:02:10 +0200

    Started database class

--- DatabaseManagementTool/DatabaseManagementTool/Classes/Database.cs
+++ DatabaseManagementTool/DatabaseManagementTool/Classes/Database.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SQLite;
 
 namespace DatabaseManagementTool.Classes
 {
     class Database
     {
+        SQLiteConnection SQLiteConnection = new SQLiteConnection($"Data Source=DefaultDB.sqlite;Version=3;");
+
+        private void CreateDatabase(string file_path)
+        {
+            SQLiteConnection.CreateFile($"@{file_path}");
+        }
     }
 }
