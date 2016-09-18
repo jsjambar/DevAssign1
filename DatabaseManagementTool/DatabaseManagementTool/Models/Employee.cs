accad0f2b4feface48ee0f0b28025ffe0fd55846
Andy Bhadai <abhadai@gmail.com>
2016-09-18 16:19:06 +0200

    Added file structure

---
+++ DatabaseManagementTool/DatabaseManagementTool/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseManagementTool
+{
+    public class Employee
+    { 
+        public int BSN { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }  
+    }
+}
---
+++ DatabaseManagementTool/DatabaseManagementTool/Models/Roles.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseManagementTool
+{
+    public class Roles
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
