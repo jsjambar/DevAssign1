bd2371423649b2f073589a69a50a085f96ffb295
Jason Sjambar <realjase@gmail.com>
2016-09-21 05:26:13 -0700

    Made a start with the project edit control

---
+++ DatabaseManagementTool/DatabaseManagementTool/EditProject.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DatabaseManagementTool
+{
+    public partial class EditProject : UserControl
+    {
+        string id = "1";
+        string name = "name";
+        string location = "somewhere";
+        string hours = "25";
+        string budget = "36";
+
+        public EditProject()
+        {
+            InitializeComponent();
+            ProjectID.Text = id;
+            ProjectName.Text = name;
+            ProjectLocation.Text = location;
+            ProjectHours.Text = hours;
+            ProjectBudget.Text = budget;
+        }
+
+        private void ProjectSave_Click(object sender, EventArgs e)
+        {
+            name = ProjectName.Text;
+            location = ProjectLocation.Text;
+            hours = ProjectHours.Text;
+            budget = ProjectBudget.Text;
+        }
+    }
+}
